3fd65d21610fa4782755a3526028addc2fcf7502
unknown <snishamal@gmail.com>
2026-03-03 04:10:17 +0530

    frontend changes

--- frontend/src/assets/component/GetStarted.jsx
+++ frontend/src/assets/component/GetStarted.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 const GetStarted = () => {
   return (
-    <div className='w-57 h-16 mt-5 bg-indigo-500 text-white px-6 py-2 rounded-lg 
-                   hover:bg-indigo-600 
-                   focus:outline-none focus:ring-2 focus:ring-indigo-400 
-                   active:bg-indigo-700 
-                   transition-colors duration-200'>
-      <div className='mt-3'> <h3 className='flex item-center justify-center text-center text-white font-bold text-base line-clamp-1'>Get Start</h3></div>
-    </div>
+    <button className='mt-10 border border-blue-500 text-white px-6 py-3 cursor-pointer rounded-[60px] mt-4 font-bold hover:bg-blue-600 hover:scale-105 transition duration-300 active:bg-blue-800 text-white px-4 py-2 rounded'>Get Started</button>
   )
 }
 
--- frontend/src/pages/Starting.jsx
+++ frontend/src/pages/Starting.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
 import GetStarted from '../assets/component/GetStarted'
 import { useNavigate } from "react-router-dom";
+import bgImage from '../assets/bd-image1.jpg'
+import { useEffect, useState } from "react";
 
 
 const Starting = () => {
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Trigger animation on mount
+    setIsLoaded(true);
+  }, []);
+
   const navigate = useNavigate();
   
   return (
-    <main>
-      <div className='w-full h-screen bg-center bg-cover absolute top-0 left-0'/>
-      <div className='px-1 py-12 xs:p-10 max-w-8xl mx-auto flex flex-col relative z-10 min-h-screen flex items-center justify-center'>
-        <header>
-          <h1 className='mx-auto max-w-7xl text-center text-5xl font-bold leading-tight tracking-[-1%] text-white sm:text-[64px] sm:leading-[76px] mb-2'>Welcome to wealth os system</h1>
-          <p className='text-center text-indigo-500 '>Your gateway to financial success starts here. Join us to manage and grow your wealth effectively</p>
-          <div 
-          onClick={() => navigate("/login")}
-          className='cursor-pointer select-none mt-4 flex item-center justify-center'><GetStarted/></div>
-          
-        </header>
-      </div>
-    </main>
+    <div className='h-screen bg-cover bg-center'
+      style={{backgroundImage: `url(${bgImage})`}}>
+        <div className="absolute inset-0 bg-gradient-to-l from-black/50 to-black/80">
+          <div className='flex items-center h-screen ml-40'>
+          <div  className={`flex justify-center px-3 overflow-hidden transition-transform transition-opacity duration-1200 ease-out 
+    ${isLoaded ? "translate-x-[0%] opacity-100" : "-translate-x-full opacity-0"}`}>
+              <div>
+                <div>
+                  <h1 className='text-blue-500 font-bold text-8xl'>Capital Sync</h1>
+                </div>
+                <div className='py-8 pr-8'>
+                  <div>
+                    <span className='text-white text-xl'>Your gateway to financial success Start here.</span>
+                  </div>
+                  <div>
+                    <span className='text-white text-xl'>Join us to manage  and grow your wealth effectively</span>
+                  </div>
+                  <div onClick={() => navigate("/login")}>
+                    <GetStarted/>
+                  </div>
+                </div>
+              </div>
+              
+          </div>
+        </div>
+        </div>
+
+    </div>
   )
 }
 
